d4ae93139b508b6736074bd2ee9ac14468f0a40b
Ricardo Hernandez <hs.ricardoj@gmail.com>
2020-11-29 19:27:29 -0500

    Added all files

---
+++ app.js
@@ -0,0 +1,50 @@
+var first = document.getElementById("first");
+var second = document.getElementById("second");
+var third = document.getElementById("third");
+var fourth = document.getElementById("fourth");
+var fifth = document.getElementById("fifth");
+var sixth = document.getElementById("sixth");
+var seventh = document.getElementById("seventh");
+var reset = document.getElementById("reset");
+
+
+first.addEventListener("click", function(){
+    document.body.style.backgroundColor = "red";
+    
+},false);
+
+second.addEventListener("click", function(){
+    document.body.style.backgroundColor = "orange";
+    
+},false);
+
+third.addEventListener("click", function(){
+    document.body.style.backgroundColor = "yellow";
+    
+},false);
+
+fourth.addEventListener("click", function(){
+    document.body.style.backgroundColor = "green";
+    
+},false);
+
+fifth.addEventListener("click", function(){
+    document.body.style.backgroundColor = "blue";
+    
+},false);
+
+sixth.addEventListener("click", function(){
+    document.body.style.backgroundColor = "indigo";
+    
+},false);
+
+
+seventh.addEventListener("click", function(){
+    document.body.style.backgroundColor = "violet";
+    
+},false);
+
+reset.addEventListener("click", function(){
+    document.body.style.backgroundColor = "white";
+    
+},false);
